e075fcc2d5fa29acc04085057d2d9a5458fd4136
Mattia-Zanini <mattzanini2@gmail.com>
2023-02-23 23:29:03 +0100

    pagina test prodotto singolo completata

--- wp-content/themes/Sandwech/pages/page-single_product-test.php
+++ wp-content/themes/Sandwech/pages/page-single_product-test.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<link href="<?php echo get_template_directory_uri(); ?>/css/cart.css" rel="stylesheet" type="text/css">
+<link href="<?php echo get_template_directory_uri(); ?>/css/single-product.css" rel="stylesheet" type="text/css">
 
 <div class="container-fluid">
     <div class="row">
@@ -10,7 +10,26 @@
                 onclick="window.location.href = 'http://localhost/sandwech-web/order'">Back</button>
         </div>
         <div class="col-9">
-            <div id="myDiv"></div>
+            <div id="myDiv">
+                <div class="row">
+                    <div class="col-7">
+                        <img class="img-fluid"
+                            src="<?php echo get_template_directory_uri(); ?>/assets/img/panini_farciti.jpg">
+                    </div>
+                    <div class="col-5">
+                        <h1 id="prod-name"></h1>
+                        <h2 id="prod-price"></h2>
+                        <h4 id="prod-description"></h4>
+                        <div class="row">
+                            <button class="col-1 btn btn-primary minus">-</button>
+                            <input type="number" value="1" class="col-2" id="prod-quantity" style="text-align: center;"
+                                min="1" max="99">
+                            <button class="col-1 btn btn-primary plus">+</button>
+                            <button class="col-2 offset-1 btn btn-danger cart">Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -22,7 +41,76 @@
     $(document).ready(function () {
         var cookieUserData = CookiesToObject(document.cookie)["userLoginData"];
         console.log(cookieUserData);
+        let url = new URLSearchParams(location.search);
+        //let prova = url.get('productID');
+        //console.log(prova);
+
+        $(".minus").click(function () {
+            let value = $("#prod-quantity").val();
+            console.log("Clicked minus: " + value);
+
+            if (value > 1) {
+                value--;
+                $("#prod-quantity").val(value);
+            }
+        });
+        $(".plus").click(function () {
+            let value = $("#prod-quantity").val();
+            console.log("Clicked plus: " + value);
+
+            if (value < 99) {
+                value++;
+                $("#prod-quantity").val(value);
+            }
+        });
+        $(".cart").click(function () {
+            let quant = $("#prod-quantity").val();
+            let usrID = cookieUserData["userID"];
+            let prodID = url.get('productID');
+
+            console.log("Clicked cart:");
+            AddItemToCart(usrID, prodID, quant);
+        });
+
+        GetProductInfo(url.get('productID'))
     });
+
+    function GetProductInfo(productID) {
+        $.ajax({
+            url: "/food-api/API/product/getProduct.php?PRODUCT_ID=" + productID,
+            type: "GET",
+            success: function (data) {
+                data = data[0];
+
+                console.log(data);
+                RenderProductInfo(data);
+            },
+            error: function (request, status, error) { }
+        });
+    }
+
+    function RenderProductInfo(info) {
+        $("#prod-name").text(info["Nome prodotto"]);
+        $("#prod-price").text(info["Prezzo"] + "€");
+        $("#prod-description").text(info["Descrizione"]);
+    }
+
+    function AddItemToCart(usr, product, quantity) {
+        //$data->prod, $data->user, $data->quantity
+        $.ajax({
+            url: "/food-api/API/cart/addProductCart.php?",
+            type: "POST",
+            data: JSON.stringify({
+                "user": usr,
+                "prod": product,
+                "quantity": quantity
+            }),
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (request, status, error) { }
+        });
+    }
 </script>
 
 <?php get_footer(); ?>
